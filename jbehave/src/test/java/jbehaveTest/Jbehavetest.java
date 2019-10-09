2354b143fcb28b380c4f9a488056192d2ba41192
shivanandchachadi <shivu.2007@gmail.com>
2019-10-09 18:28:47 +0530

    first commit

--- jbehave/src/test/java/jbehaveTest/Jbehavetest.java
+++ jbehave/src/test/java/jbehaveTest/Jbehavetest.java
@@ -1,6 +1,9 @@
 package jbehaveTest;
+import org.apache.commons.digester.plugins.strategies.LoaderFromClass;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.reporters.Format;
 //import org.jbehave.core.junit.JUnitStories;
 //import org.jbehave.core.reporters.Format;
@@ -8,15 +11,18 @@ import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 //import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.List;
 
 import net.serenitybdd.jbehave.SerenityStories;
+import stepdef.Definationclass;
 //import net.serenitybdd.junit.runners.SerenityRunner;
 
 
-//@RunWith(SerenityRunner.class) 
+//@RunWith(SerenityReportingRunner.class) 
+
 public class Jbehavetest extends SerenityStories {
 
 	
@@ -30,17 +36,18 @@ public class Jbehavetest extends SerenityStories {
     public Configuration configuration() {
         return new MostUsefulConfiguration()
                 // Configure the story report
+        		.useStoryLoader(new LoadFromClasspath(this.getClass()))
                 .useStoryReporterBuilder(new StoryReporterBuilder()
                         // Post the report to the console
-                        .withFormats(Format.STATS));
+                		//.withCodeLocation(codeLocationFromClass(this.getClass()))
+                        .withFormats(Format.CONSOLE,Format.TXT,Format.XML,Format.HTML));
+                		//.withDefaultFormats().withFormats(Format.CONSOLE));
     }
 
     // Specify a feature file
     public List<String> storyPaths() {
         return Arrays.asList("./res/jbehave.story");
     }
-
-	
 
 	 
 	
